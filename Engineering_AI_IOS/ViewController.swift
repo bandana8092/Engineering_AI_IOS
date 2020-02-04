7407722195dc788b6fd9911edab410c99911b180
Ojas Innovative Technologies Pvt Ltd <ojas.bitbucket@gmail.com>
2020-02-04 15:03:14 +0530

    Initial Commit

---
+++ Engineering_AI_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Engineering_AI_IOS
+//
+//  Created by Swaminath Kosetty on 04/02/20.
+//  Copyright © 2020 Ojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Engineering_AI_IOSTests/Engineering_AI_IOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Engineering_AI_IOSTests.swift
+//  Engineering_AI_IOSTests
+//
+//  Created by Swaminath Kosetty on 04/02/20.
+//  Copyright © 2020 Ojas. All rights reserved.
+//
+
+import XCTest
+@testable import Engineering_AI_IOS
+
+class Engineering_AI_IOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
